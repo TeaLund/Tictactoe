e43b5a974c16b5f4b9d6888d56b321f01786188f
BMedkouri <Bilal.Medkouri@gmail.com>
2018-03-23 15:09:21 +0100

    Main Menu
    Added main menu

---
+++ TicTacToe/Assets/Scripts/MenuController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour {
+
+    public Button startGameButton;
+    public Button quitGameButton;
+
+	public void StartGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
